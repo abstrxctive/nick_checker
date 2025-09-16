8ba4bd945ede8e9acfe5869ec01c34885322a116
abstrxctive <murisa833@gmail.com>
2025-09-16 23:16:37 +0300

    Create urls.py

---
+++ urls.py
@@ -0,0 +1,12 @@
+urls = [
+    "https://t.me/",
+    "https://x.com/",
+    "https://vk.com/",
+    "https://love.ru/",
+    "https://github.com/",
+    "https://tiktok.com/@",
+    "https://ok.ru/profile/",
+    "https://www.avito.ru/user/",
+    "https://www.instagram.com/",
+    "https://www.facebook.com/profile.php?id=",
+]
